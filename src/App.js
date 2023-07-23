33d1992a4190723081435c468480605a2f181a6e
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-07-23 01:08:26 +0100

    Add a container to the main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Header from './Components/Header';
+import "./App.css";
+import Header from "./Components/Header";
+import InputContainer from "./Components/InputContainer";
 
 function App() {
   return (
     <div className="App">
-<Header />
+      <Header />
+      <InputContainer />
     </div>
   );
 }
---
+++ src/Components/InputContainer.js
@@ -0,0 +1,9 @@
+const InputContainer = () => {
+  return (
+    <div>
+      <p>Here you can see the text</p>
+      <button>Click here ➤</button>
+    </div>
+  );
+};
+ export default InputContainer;
